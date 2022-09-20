e6d48fdec23afc8efec3b76e6de37cd547331070
Ahmed_Arafat10 <ahmedxarafat0101@gmail.com>
2022-09-20 20:12:39 +0200

    finishing vector file

--- Vector.cpp
+++ Vector.cpp
@@ -1,92 +1,151 @@
 #include <bits/stdc++.h>
 typedef long long ll;
-
 using namespace std;
 
 int main()
 {
-    //vector<ll> v1
-  // vector<ll> v1 ;
-   vector<ll> v1={12,3,4};
-   //v1.push_back(1);
-   //vector<ll> v1(5,200);
-   //v1.push_back(10);
-   //v1.emplace_back(10);
-   // v1.pop_back();
-  // cout<<v1.front();
-   //cout<<v1.back();
-    // cout<<v1.at(0);
- // cout<<v1[0];
 
-   //vector<ll>::iterator it;
-   vector<ll>::reverse_iterator rit;
+    //Declare
+    vector<ll> v1;
+
+    //Initialize
+    vector<ll> v2 = {12,3,4};
+
+    //Add new element at end
+    v2.push_back(1);
+
+    //Faster then push_back(), not supported by all compilers
+    v2.emplace_back(10);
+
+    // Add number 200 five times
+    vector<ll> v3(5,200);
+
+    //Remove last elemet
+    v2.pop_back();
+
+    //Print first element
+    cout<<v2.front();
+
+    //Print last element
+    cout<<v2.back();
+
+    //Print element at specific index
+    cout<<v2.at(0);
+    cout<<v2[0];
+
+    //Declare an iterator (pointer)
+    vector<ll>::iterator it;
+
+    // use -1 as end() refers to }
+    it = v1.end()-1; // it iterator points on last element
+    it = v1.begin(); // it iterator points on first element
+
+    //Declare & Initialize an iterator pointing to first element
+    vector<ll>::iterator it2 = v1.begin();
+
+    //Declare a reverse iterator (pointer)
+    vector<ll>::reverse_iterator rit;
     vector<ll>::reverse_iterator rit2;
-  // vector<ll> v1::iterator it2 = v1.begin();
-  // it = v1.end();// use -1 as it refers to }
-  // it = v1.rbegin() ;
-//   it = v1.begin();
-//   rit = v1.rend()-2;
-//   rit2 = v1.rbegin()+1;
-//   cout<<*rit2;
-//cout<<v1[0];
-//-------
 
-//for(ll i=0;i<v1.size();i++) cout<<v1[i]<<" " ;
-//cout<<"\n";
-//    it = v1.begin();
-//
-//    for(;it != v1.end();it++) cout<<*it<<" " ;
-//
-//    auto ii = v1.begin();
-//    auto str = "hello";
-//    cout<<*ii;
-//
-//    for(auto it : v1)//Foreach
-//        cout<<it<<" ";
+    //rbegin() will point on last element
+    //Note to move to the left you will use [+1] like v2.rbegin()+1
+    //Point to last element
+    rit = v2.rbegin();
+    //Point to second last element
+    rit = v2.rbegin()+1;
 
+    //rend() will point on {  [before first eleemt]
+    //Note to move to the right you will use [-1] like v2.rend()-1
+    //Point to first element
+    rit2 = v2.rend()-1;
 
+    //Print element iterator is pointing at (use dereferencing operator to access element stored in the address that is stored in the iterator)
+    cout<<*rit2;
 
+    //Check if vector is empty
+    cout<<v1.empty();
 
- //vector<ll> v2={2,1,3,4},v3={10,11,12,13};
-// //v2.insert(v2.begin()+3,14);
-// v2.insert(v2.end()-1,14);O(N)
-// cout<<v2[4];
-// v2.swap(v3);
-// for(auto it:v2) cout<<it<<" ";
+    //Print number of element stored
+    cout<<v1.size();
 
+    //Iterate over all elements in the vector [Method 1]
+    for(ll i = 0; i<v1.size(); i++)
+        cout<<v1[i]<<" " ;
+    puts("");
 
-//v2.erase(v2.begin(),v2.begin()+1); O(N)
-//v2.erase(v2.begin(),v2.end());
-//for(auto it:v2) cout<<it<<" ";
+    //Iterate over all elements in the vector [Method 2]
+    //auto it5 = v1.begin();
+    for(auto it5 = v1.begin(); it5 != v1.end(); it5++)
+        cout<<*it5<<" " ;
+    puts("");
 
-//    v2.clear();
-
-
-//reverse(v2.begin(),v2.end()); O(N)
+    //Iterate over all elements in the vector [Method 3]
+    //Foreach
+    for(auto &it : v1)
+        cout<<it<<" ";
+    puts("");
 
 
-//sort(v2.begin(),v2.end());
-//sort(v2.rbegin(),v2.rend());
-//sort(v2.begin(),v2.end(),greater<ll>()); O(n log n)
+    //Create two vectors
+    vector<ll> v5= {2,1,3,4}, v6= {10,11,12,13};
+    //Swap elements between two vectors
+    v5.swap(v6);
+
+    vector<ll> v7= {1,3,4};
+
+    //Insert at index 0 element 14
+    v7.insert(v7.begin(),14);
+
+    //Insert at index 1 element 14
+    v7.insert(v7.begin()+1,15);
+
+    //Insert at last element 14
+    v7.insert(v7.end(),98); //O(N)
+
+    //Insert at last-1 element 14
+    v7.insert(v7.end()-1,99);
+
+    //Erase from index 0 till index 1 [1 is excluded VIP Point]
+    v7.erase(v7.begin(),v7.begin()+1);// O(N)
+
+    //Erase from index 0 till last element [last element will be excluded]
+    v7.erase(v7.begin(),v7.end()-1);
+
+    //Remove all elements
+    v2.clear();
+
+    //Reverse a vector
+    reverse(v2.begin(),v2.end());//O(N)
+
+    //Sort a vector in Ascending order
+    sort(v7.begin(),v7.end());
+
+    //Sort a vector in Descending order
+    sort(v7.rbegin(),v7.rend());
+    sort(v7.begin(),v7.end(),greater<ll>()); //O(n log n)
 
 
+    vector<ll> v10{3,5,10,15};
+    auto it10 = v10.begin();
 
-//vector<ll> v2={2,1,3,4},v3={10,11,12,13};
+    //Access first element then increment it
+    cout<<++(*it10);//4
 
-//auto it = v2.begin();
-//cout<<++(*it);
-//cout<<*(it++);
-//cout<<*(--it);
-//for(auto it:v2) cout<<it<<" ";
+    //Increment to second element (but it10 expression will still refer to first element) then access it
+    cout<<*(it10++);//3
 
-
-//    cout<<v1.max_size()<<"\n";
-//     cout<<v1.capacity();
-// v1.size()
-//    v1.shrink_to_fit();
-  //  cout<<v1.empty();
+    //Increment to second element then access it
+    cout<<*(++it10);//5
 
 
+    //Print Maximum possible elements to be stored
+    cout<<v2.max_size()<<"\n";
+
+    //Print size of the storage space currently allocated for the vector
+    cout<<v1.capacity();
+
+    //Reduces the capacity of the container to fit its size and destroys all elements beyond the capacity
+    v1.shrink_to_fit();
 
     return 0;
 }
